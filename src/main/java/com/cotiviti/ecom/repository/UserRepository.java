8548f52691d26b48c67f7d97b602bbb2897a22f4
Kushal Upreti <kushalupreti101@gmail.com>
2022-12-17 09:40:51 +0545

    Merged frontend and backend

--- src/main/java/com/cotiviti/ecom/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.cotiviti.ecom.repository;
-
-import com.cotiviti.ecom.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User,Integer> {
-}
